f9a86fd65cca7d322575d229ec67a8f3f19bfd0a
mhamedGd <mhamed.m14@gmail.com>
2022-12-22 22:02:41 +0300

    Add side panel

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,3 +1,14 @@
+import { DOCUMENTATION } from "./documentation_information.mjs";
+
+function GetDocID(name) {
+    for(let i = 0; i < DOCUMENTATION.length; i++){
+        if(DOCUMENTATION[i].class == name){
+            return i;
+        }
+    }
+}
+
+
 function CreateNavBar(){
     /*
     <nav class="navbar-container">
@@ -22,14 +33,22 @@ function CreateNavBar(){
     NAV.appendChild(NAV_LABEL);
 
     const NAV_LABEL_LINK = document.createElement('a');
+    //NAV_LABEL_LINK.innerHTML = '<b>Playground2D</b>';
     NAV_LABEL_LINK.innerHTML = '<b>Playground2D</b>';
     NAV_LABEL_LINK.setAttribute('href', './index.html');
     NAV_LABEL_LINK.classList.add('navbar-link');
     NAV_LABEL.appendChild(NAV_LABEL_LINK);
 
+    const BURGER_BUTTON_CONTAINER = document.createElement('div');
+    BURGER_BUTTON_CONTAINER.classList.add('center-content');
+    NAV.appendChild(BURGER_BUTTON_CONTAINER);
+
     const BURGER_BUTTON = document.createElement('button');
     BURGER_BUTTON.classList.add('navbar-item', 'navbar-button');
-    NAV.appendChild(BURGER_BUTTON);
+    BURGER_BUTTON.addEventListener('click', ()=>{
+        ActivateMenu();
+    });
+    BURGER_BUTTON_CONTAINER.appendChild(BURGER_BUTTON);
 
 
     const BURGER_LINE1 = document.createElement('span');
@@ -43,10 +62,33 @@ function CreateNavBar(){
     const BURGER_LINE3 = document.createElement('span');
     BURGER_LINE3.classList.add('burger-line');
     BURGER_BUTTON.appendChild(BURGER_LINE3);
+
+    const DOCS_MENU = document.createElement('div');
+    DOCS_MENU.classList.add('docs-menu');
+    document.querySelector('body').appendChild(DOCS_MENU);
+}
+
+function CreateDocsInfo() {
+
 }
 
 CreateNavBar();
 
+const DOCUMENTATION_BUTTON = document.querySelector('#documenation-button');
+if(DOCUMENTATION_BUTTON != null) {
+    DOCUMENTATION_BUTTON.addEventListener('click', ()=>{
+        localStorage.setItem('DocIndex', JSON.stringify(0));
+    });
+}
+
 function GetDocumentation() {
     localStorage.setItem('DocIndex', JSON.stringify(0));
+}
+
+function ActivateMenu() {
+    const ACTIVE_CLASS = 'active';
+    const BURGER_BUTTON = document.querySelector('.navbar-button');
+    const DOCS_MENU = document.querySelector('.docs-menu');
+    BURGER_BUTTON.classList.toggle(ACTIVE_CLASS);
+    DOCS_MENU.classList.toggle(ACTIVE_CLASS);
 }
